024e9fe22f549cdf06f006c7a50d0cbce55d4e17
this-username-better-be-available <143838012+this-username-better-be-available@users.noreply.github.com>
2023-09-03 13:42:39 -0500

    added subtraction

--- calculator.cpp
+++ calculator.cpp
@@ -6,16 +6,27 @@ int main() {
 	std::cout << "input what you would like to do. (addition: 1. nothing else is working)";
 	std::cin >> input;
 	if (input == 1) {
-	int a;
-	int b;
+	int aa;
+	int ab;
 	int sum;
 	std::cout << "addition input number 1";
-	std::cin >> a;
+	std::cin >> aa;
 	std::cout << "input number 2";
-	std::cin >> b;
-	sum = a + b;
+	std::cin >> ab;
+	sum = aa + ab;
 	std::cout << "sum is: " << sum;
-	} else {
+} else { if (input == 2) {
+int sa;
+int sb;
+int answer;
+std::cout << "subtraction input number 1: ";
+std::cin >> sa;
+std::cout << "input number 2: ";
+std::cin >> ab;
+int answer = sa - sb;
+std::cout << "answer is: " << answer;
+} else {
 		std::cout << "nothing else is working";
 	}
+ }
 }
