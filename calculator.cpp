fb3176e7072e2558043e34fb39321b1293e2cc9f
this-username-better-be-available <143838012+this-username-better-be-available@users.noreply.github.com>
2023-09-18 15:42:55 -0500

    Update calculator.cpp
    shortened code, changed instructions

--- calculator.cpp
+++ calculator.cpp
@@ -3,7 +3,7 @@
 
 int main() {
 	int input;
-	std::cout << "input what you would like to do. (addition: 1. nothing else is working)";
+	std::cout << "input what you would like to do. (addition: 1. subtraction: 2. nothing else is working)";
 	std::cin >> input;
 	if (input == 1) {
 	int aa;
@@ -18,7 +18,6 @@ int main() {
 } else { if (input == 2) {
         int sa;
         int sb;
-        int answer;
         std::cout << "subtraction input number 1: ";
         std::cin >> sa;
         std::cout << "input number 2: ";
