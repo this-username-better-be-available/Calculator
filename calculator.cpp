0490732cf4f5a6058c98cf92f229a07952ebf50b
this-username-better-be-available <143838012+this-username-better-be-available@users.noreply.github.com>
2024-01-23 10:41:42 -0600

    various small fixes

--- calculator.cpp
+++ calculator.cpp
@@ -3,18 +3,19 @@
 
 int main() {
 	int input;
-	std::cout << "Input what you would like to do. (Addition: 1. Subtraction: 2. Multiplication: 3. Division: 4)";
+	std::cout << "Input what you would like to do. (Addition: 1. Subtraction: 2. Multiplication: 3. Division: 4) ";
 	std::cin >> input;
 	if (input == 1){
 	int aa;
 	int ab;
 	int sum;
-	std::cout << "addition input number 1";
+	std::cout << "addition input number 1: ";
 	std::cin >> aa;
-	std::cout << "input number 2";
+	std::cout << "input number 2: ";
 	std::cin >> ab;
 	sum = aa + ab;
 	std::cout << "sum is: " << sum;
+        std::cout << " ";
 } else if (input == 2) {
         int sa;
         int sb;
@@ -24,6 +25,7 @@ int main() {
         std::cin >> sb;
         int answer = sa - sb;
         std::cout << "answer is: " << answer;
+        std::cout << " ";
 } else if (input == 3) {
         int ma;
         int mb;
@@ -33,6 +35,7 @@ int main() {
         std::cin >> mb;
         int answer = ma * mb;
         std::cout << "answer is: " << answer;
+        std::cout << " ";
 } else if (input == 4) {
 	int da;
         int db;
@@ -42,5 +45,9 @@ int main() {
         std::cin >> db;
         int answer = da / db;
         std::cout << "answer is: " << answer;
-	}
+        std::cout << " ";
+	} else {
+        std::cout << "Wrong number ";
+        std::cout << " ";
+    }
  }
